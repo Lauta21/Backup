6f549dce5b8f54822bdc2968061afbe8bd9a9fbd
Lauta21 <42208093+Lauta21@users.noreply.github.com>
2018-08-17 11:17:34 -0300

    Backup

---
+++ OficinaEmpleo/src/Poo/Muni/Clase.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo.muni;
+
+/**
+ *
+ * @author Capacitacion5
+ */
+public class Clase {
+    
+    private String Nombre;
+    private String Apellido;
+    private String NombreUsuario;
+    private String Contraseña;
+    private String CofirmarContraseña;
+    private String Email;
+
+    public Clase() {
+    }
+
+    public Clase(String Nombre, String Apellido, String NombreUsuario, String Contraseña, String CofirmarContraseña, String Email) {
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.NombreUsuario = NombreUsuario;
+        this.Contraseña = Contraseña;
+        this.CofirmarContraseña = CofirmarContraseña;
+        this.Email = Email;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+    public String getApellido() {
+        return Apellido;
+    }
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+    public String getNombreUsuario() {
+        return NombreUsuario;
+    }
+    public void setNombreUsuario(String NombreUsuario) {
+        this.NombreUsuario = NombreUsuario;
+    }
+    public String getContraseña() {
+        return Contraseña;
+    }
+    public String getCofirmarContraseña() {
+        return CofirmarContraseña;
+    }
+    public void setCofirmarContraseña(String CofirmarContraseña) {
+        this.CofirmarContraseña = CofirmarContraseña;
+    }
+    public String getEmail() {
+        return Email;
+    }
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+    
+    
+    
+}
